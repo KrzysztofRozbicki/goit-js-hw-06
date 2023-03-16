5261a6720994563c19ad188d71017d88676822b4
Krzysztof R <plkrozbicki@gmail.com>
2023-03-17 00:11:27 +0100

    Task 6 equals bugfix

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,7 @@
 const inputEl = document.querySelector('#validation-input');
 
 inputEl.addEventListener('blur', () => {
-  inputEl.className = '';
-  console.log(inputEl.value.length, inputEl.dataset.length);
   inputEl.value.length === +inputEl.dataset.length
-    ? inputEl.classList.toggle('valid')
-    : inputEl.classList.toggle('invalid');
+    ? (inputEl.className = 'valid')
+    : (inputEl.className = 'invalid');
 });
