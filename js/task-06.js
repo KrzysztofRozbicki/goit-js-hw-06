f36b7094c7f2283c4afe7352afe55b6d14147f7c
Krzysztof R <plkrozbicki@gmail.com>
2023-03-19 10:43:56 +0100

    update task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,7 @@
 const inputEl = document.getElementById('validation-input');
 
 inputEl.addEventListener('blur', () => {
-  inputEl.value.length === +inputEl.dataset.length
+  inputEl.value.length === Number(inputEl.dataset.length)
     ? (inputEl.className = 'valid')
     : (inputEl.className = 'invalid');
 });
